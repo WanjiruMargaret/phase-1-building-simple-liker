b2b8754a1b64ff0f8b91c2667b24c3be6bcf7e26
WanjruMargaret <maggiesamwells@gmail.com>
2025-06-26 11:04:24 +0300

    test passed

--- main.js
+++ main.js
@@ -3,6 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.getElementById("modal");
+errorModal.classList.add("hidden");
+
+// Select all like glyphs
+const hearts = document.querySelectorAll(".like-glyph");
+
+hearts.forEach(heart => {
+  heart.addEventListener("click", () => {
+    // Simulate server call
+    mimicServerCall()
+      .then(() => {
+        // Success: toggle heart state
+        if (heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add("activated-heart");
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove("activated-heart");
+        }
+      })
+      .catch(error => {
+        // Failure: show error modal
+        const modalMessage = document.getElementById("modal-message");
+        modalMessage.innerText = error;
+        errorModal.classList.remove("hidden");
+
+        // Hide modal after 3 seconds
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
 
 
 
